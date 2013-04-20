0e10b66ccfc2010f314840a90af19926415721b9
madguy <madguy@drumcan.in>
2013-04-20 17:26:26 +0900

    自分用にリファクタリング

--- jquery.vgrid.js
+++ jquery.vgrid.js
@@ -1,4 +1,4 @@
-/**
+/*!
  * jQuery VGrid v0.1.9 - variable grid layout plugin
  *
  * Terms of Use - jQuery VGrid
@@ -7,379 +7,337 @@
  * Copyright 2009-2012 xlune.com All rights reserved.
  * (http://blog.xlune.com/2009/09/jqueryvgrid.html)
  */
-(function($)
-{
-	function makePos(self)
-	{
-		var _childs = self.data("_vgchild");
-		var _width = self.width();
-		var _matrix = [[0,_width,0]];
-		var _hmax=0, _c, _size, _point;
-		_childs.each(function(i)
-		{
-			_c = $(this);
-			if(_c.css("display") == "none"){
-				return true;
-			}
-			_size = getSize(_c);
-			_point = getAttachPoint(_matrix, _size[0]);
-			_matrix = updateAttachArea(_matrix, _point, _size);
-			_hmax = Math.max(_hmax, _point[1] + _size[1]);
-			_c.data("_vgleft", _point[0]);
-			_c.data("_vgtop", _point[1]);
-		});
-		self.data("_vgwrapheight", _hmax);
-		heightTo(self);
-	};
-	function getAttachPoint(mtx, width)
-	{
-		var _mtx = mtx.concat().sort(matrixSortDepth);
-		var _max = _mtx[_mtx.length-1][2];
-		for(var i=0,imax=_mtx.length; i<imax; i++)
-		{
-			if(_mtx[i][2] >= _max) break;
-			if(_mtx[i][1]-_mtx[i][0] >= width)
-			{
-				return [_mtx[i][0], _mtx[i][2]];
-			}
-		}
-		return [0, _max];
-	};
-	function updateAttachArea(mtx, point, size)
-	{
-		var _mtx = mtx.concat().sort(matrixSortDepth);
-		var _cell = [point[0], point[0]+size[0], point[1]+size[1]];
-		for(var i=0,imax=_mtx.length; i<imax; i++)
-		{
-			if(_cell[0] <= _mtx[i][0] && _mtx[i][1] <= _cell[1])
-			{
-				delete _mtx[i];
-			}
-			else
-			{
-				_mtx[i] = matrixTrimWidth(_mtx[i], _cell);
-			}
-		}
-		return matrixJoin(_mtx, _cell);
-	};
-	function matrixSortDepth(a, b)
-	{
-		if(!a || !b) return 0;
-		return ((a[2] == b[2] && a[0] > b[0]) || a[2] > b[2]) ? 1 : -1;
-	};
-	function matrixSortX(a, b)
-	{
-		if(!a || !b) return 0;
-		return (a[0] > b[0]) ? 1 : -1;
-	};
-	function matrixJoin(mtx, cell)
-	{
-		var _mtx = mtx.concat([cell]).sort(matrixSortX);
-		var _mtx_join = [];
-		for(var i=0,imax=_mtx.length; i<imax; i++)
-		{
-			if(!_mtx[i]) continue;
-			if(_mtx_join.length > 0
-				&& _mtx_join[_mtx_join.length-1][1] == _mtx[i][0]
-				&& _mtx_join[_mtx_join.length-1][2] == _mtx[i][2])
-			{
-				_mtx_join[_mtx_join.length-1][1] = _mtx[i][1];
-			}
-			else
-			{
-				_mtx_join.push(_mtx[i]);
-			}
-		}
-		return _mtx_join;
-	};
-	function matrixTrimWidth(a, b)
-	{
-		if(a[0] >= b[0] && a[0] < b[1] || a[1] >= b[0] && a[1] < b[1])
-		{
-			if(a[0] >= b[0] && a[0] < b[1])
-			{
+(function($) {
+	var matrixTrimWidth = function(a, b) {
+		if (a[0] >= b[0] && a[0] < b[1] || a[1] >= b[0] && a[1] < b[1]) {
+			if (a[0] >= b[0] && a[0] < b[1]) {
 				a[0] = b[1];
-			}
-			else
-			{
+			} else {
 				a[1] = b[0];
 			}
 		}
 		return a;
 	};
-	function getSize(child)
-	{
-		var _w = child.width();
-		var _h = child.height();
-		_w += Number(child.css("margin-left").replace('px', ''))
-				+Number(child.css("padding-left").replace('px', ''))
-				+Number(child.get(0).style.borderLeftWidth.replace('px', ''))
-				+Number(child.css("margin-right").replace('px', ''))
-				+Number(child.css("padding-right").replace('px', ''))
-				+Number(child.get(0).style.borderRightWidth.replace('px', ''));
-		_h += Number(child.css("margin-top").replace('px', ''))
-				+Number(child.css("padding-top").replace('px', ''))
-				+Number(child.get(0).style.borderTopWidth.replace('px', ''))
-				+Number(child.css("margin-bottom").replace('px', ''))
-				+Number(child.css("padding-bottom").replace('px', ''))
-				+Number(child.get(0).style.borderBottomWidth.replace('px', ''));
-		return [_w, _h];
+
+	var getSize = function($child) {
+		var w = $child.outerWidth(true);
+		var h = $child.outerHeight(true);
+		return [w, h];
 	};
-	function heightTo(self)
-	{
-		var _self = self;
-		var _delay = _self.data("_vgchild").length
-			* (_self.data("_vgopt").delay || 0)
-			+ _self.data("_vgopt").time || 500;
-		_self.stop();
-		if(_self.height() < _self.data("_vgwrapheight"))
-		{
-			if($.browser.msie)
-			{
-				_self.height(_self.data("_vgwrapheight"));
-			}
-			else
-			{
-				_self.animate(
-					{
-						height: _self.data("_vgwrapheight")+"px"
-					},
-					(_self.data("_vgopt").time || 500),
-					"easeOutQuart"
-				);
-			}
-		}
-		else
-		{
-			clearTimeout(_self.data("_vgwraptimeout"));
-			_self.data("_vgwraptimeout", setTimeout(function(){
-				if($.browser.msie)
-				{
-					_self.height(_self.data("_vgwrapheight"));
-				}
-				else
-				{
-					_self.animate(
-						{
-							height: _self.data("_vgwrapheight")+"px"
-						},
-						(_self.data("_vgopt").time || 500),
-						"easeOutQuart"
-					);
-				}
-			}, _delay));
+
+	var heightTo = function($self) {
+		var delay = $self.data('_vgchild').length * ($self.data('_vgopt').delay || 0) + $self.data('_vgopt').time || 500;
+		$self.stop();
+		var height = $self.data('_vgwrapheight');
+		if ($self.height() < height) {
+			$self.height($self.data('_vgwrapheight'));
+		} else {
+			clearTimeout($self.data('_vgwraptimeout'));
+			$self.data('_vgwraptimeout', setTimeout(function() {
+				$self.height(height);
+			}, delay));
 		}
 	};
-	function moveTo(childs)
-	{
-		var _c;
-		childs.each(function(i)
-		{
-			_c = $(this);
-			_c.css("left", _c.data("_vgleft")+"px");
-			_c.css("top", _c.data("_vgtop")+"px");
+
+	var moveTo = function($childs) {
+		$childs.each(function(i) {
+			var $child = $(this);
+			if ($.support.transition) {
+				$child.css('x', $child.data('_vgleft') + 'px');
+				$child.css('y', $child.data('_vgtop') + 'px');
+			} else {
+				$child.css('left', $child.data('_vgleft') + 'px');
+				$child.css('top', $child.data('_vgtop') + 'px');
+			}
 		});
 	};
-	function animateTo(childs, easing, time, delay)
-	{
-		var _self = $(childs).parent();
+
+	var animateTo = function($childs, easing, time, delay) {
+		var $self = $childs.parent();
 		var isMove = false;
-		var imax = childs.length;
-		var i,_c,_pos;
-		for(i=0; i<imax; i++)
-		{
-			_c = $(childs[i]);
-			_pos = _c.position();
-			if(_pos.left != _c.data("_vgleft") || _pos.top != _c.data("_vgtop"))
-			{
+		var imax = $childs.length;
+		var $c;
+		var pos;
+		for (var i = 0; i < imax; i++) {
+			$c = $($childs[i]);
+			pos = $c.position();
+			if (pos.left != $c.data('_vgleft') || pos.top != $c.data('_vgtop')) {
 				isMove = true;
 			}
 		}
-		if(isMove)
-		{
-			if(typeof(_self.data("_vgopt").onStart) == "function") _self.data("_vgopt").onStart();
-			childs.each(function(i)
-			{
-				var _c = $(this);
-				var _opt = {
+		if (isMove) {
+			$self.trigger('vgrid_start');
+
+			$childs.each(function(i) {
+				var $child = $(this);
+				var opt = {
 					duration: time,
 					easing: easing
 				};
-				if(childs.size()-1 == i)
-				{
-					_opt.complete = _self.data("_vgopt").onFinish || null;
+				if ($childs.size() - 1 === i) {
+					opt.complete = function() {
+						$self.trigger('vgrid_finish');
+					};
 				}
-				clearTimeout(_c.data("_vgtimeout"));
-				_c.data("_vgtimeout", setTimeout(function(){
-					_c.animate(
-						{
-							left: _c.data("_vgleft")+"px",
-							top: _c.data("_vgtop")+"px"
-						},
-						_opt
-					);
-				}, i*delay));
+				clearTimeout($child.data('_vgtimeout'));
+				$child.data('_vgtimeout', setTimeout(function() {
+					if ($.support.transition) {
+						$child.transition($.extend({}, opt, {
+							x: $child.data('_vgleft') + 'px',
+							y: $child.data('_vgtop') + 'px'
+						}));
+					} else {
+						$child.animate({
+							left: $child.data('_vgleft') + 'px',
+							top: $child.data('_vgtop') + 'px'
+						}, opt);
+					}
+				}, i * delay));
 			});
 		}
 	};
-	function refreshHandler(tg)
-	{
-		tg.each(function(num){
-			var _self = $(this);
-			clearTimeout(_self.data("_vgtimeout"));
-			_self.data("_vgtimeout", setTimeout(function(){
-				makePos(_self);
-				animateTo(
-					_self.data("_vgchild"),
-					_self.data("_vgopt").easing || "linear",
-					_self.data("_vgopt").time || 500,
-					_self.data("_vgopt").delay || 0
-				);
+
+	var refreshHandler = function($tg) {
+		$tg.each(function(num) {
+			var $self = $(this);
+			clearTimeout($self.data('_vgtimeout'));
+			$self.data('_vgtimeout', setTimeout(function() {
+				makePos($self);
+
+				var opt = $self.data('_vgopt');
+				var easing = opt.easing || 'linear';
+				var time = opt.time || 500;
+				var delay = opt.delay || 0;
+				animateTo($self.data('_vgchild'), easing, time, delay);
 			}, 500));
 		});
 	};
-	function setFontSizeListener(self, func)
-	{
-		var s = $("<span />")
-			.text(" ")
-			.attr("id", "_vgridspan")
-			.hide()
-			.appendTo("body");
-		s.data("size", s.css("font-size"));
-		s.data("timer", setInterval(function(){
-			if(s.css("font-size") != s.data("size"))
-			{
-				s.data("size", s.css("font-size"));
+
+	var makePos = function($self) {
+		var $childs = $self.data('_vgchild');
+		var width = $self.width();
+		var matrix = [
+			[0, width, 0]
+		];
+		var hmax = 0;
+		$childs.each(function(i) {
+			var $c = $(this);
+			var size = getSize($c);
+			var point = getAttachPoint(matrix, size[0]);
+			matrix = updateAttachArea(matrix, point, size);
+			hmax = Math.max(hmax, point[1] + size[1]);
+			$c.data('_vgleft', point[0]);
+			$c.data('_vgtop', point[1]);
+		});
+		$self.data('_vgwrapheight', hmax);
+		heightTo($self);
+	};
+
+	var getAttachPoint = function(mtx, width) {
+		var mtx = mtx.concat().sort(matrixSortDepth);
+		var max = mtx[mtx.length - 1][2];
+		for (var i = 0, imax = mtx.length; i < imax; i++) {
+			if (mtx[i][2] >= max) {
+				break;
+			}
+			if (mtx[i][1] - mtx[i][0] >= width) {
+				return [mtx[i][0], mtx[i][2]];
+			}
+		}
+		return [0, max];
+	};
+
+	var updateAttachArea = function(mtx, point, size) {
+		var mtx = mtx.concat().sort(matrixSortDepth);
+		var cell = [point[0], point[0] + size[0], point[1] + size[1]];
+		for (var i = 0, imax = mtx.length; i < imax; i++) {
+			if (cell[0] <= mtx[i][0] && mtx[i][1] <= cell[1]) {
+				delete mtx[i];
+			} else {
+				mtx[i] = matrixTrimWidth(mtx[i], cell);
+			}
+		}
+		return matrixJoin(mtx, cell);
+	};
+	var matrixSortDepth = function(a, b) {
+		if (!a || !b) {
+			return 0;
+		}
+		return ((a[2] == b[2] && a[0] > b[0]) || a[2] > b[2]) ? 1 : -1;
+	};
+
+	var matrixSortX = function(a, b) {
+		if (!a || !b) {
+			return 0;
+		}
+		return (a[0] > b[0]) ? 1 : -1;
+	};
+
+	var matrixJoin = function(mtx, cell) {
+		var mtx = mtx.concat([cell]).sort(matrixSortX);
+		var mtx_join = [];
+		for (var i = 0, imax = mtx.length; i < imax; i++) {
+			if (!mtx[i]) {
+				continue;
+			}
+			if (mtx_join.length > 0 && mtx_join[mtx_join.length - 1][1] == mtx[i][0] && mtx_join[mtx_join.length - 1][2] == mtx[i][2]) {
+				mtx_join[mtx_join.length - 1][1] = mtx[i][1];
+			} else {
+				mtx_join.push(mtx[i]);
+			}
+		}
+		return mtx_join;
+	};
+
+	var setFontSizeListener = function(self, func) {
+		var s = $('<span />').text(' ').prop('id', '_vgridspan').hide().appendTo('body');
+		s.data('size', s.css('font-size'));
+		s.data('timer', setInterval(function() {
+			if (s.css('font-size') != s.data('size')) {
+				s.data('size', s.css('font-size'));
 				func(self);
 			}
 		}, 1000));
 	};
-	function setImgLoadEvent(self, func)
-	{
-		if(!self.data("vgrid-image-event-added")){
-			self.data("vgrid-image-event-added", 1);
-			self.bind("vgrid-added", function(){
-				self.find("img").each(function(){
-					var img = $(this);
-					if(!img.data("vgrid-image-handler")){
-						img.data("vgrid-image-handler", 1);
-						img.bind("load", function(){
-							func(self);
+
+	var setImgLoadEvent = function($self, func) {
+		if (!$self.data('vgrid-image-event-added')) {
+			$self.data('vgrid-image-event-added', 1);
+			$self.on('vgrid-added', function() {
+				$self.find('img').each(function() {
+					var $img = $(this);
+					if (!$img.data('vgrid-image-handler')) {
+						$img.data('vgrid-image-handler', 1);
+						$img.on('load', function() {
+							func($self);
 						});
 					}
 				});
 			});
 		}
-		self.trigger("vgrid-added");
-		var _append = self.append;
-		var _prepend = self.prepend;
-		self.append = function(){
-			_append.apply(self, arguments);
-			self.trigger("vgrid-added");
+		$self.triggerHandler('vgrid-added');
+		var append = $self.append;
+		var prepend = $self.prepend;
+		$self.append = function() {
+			append.apply($self, arguments);
+			$self.triggerHandler('vgrid-added');
 		};
-		self.prepend = function(){
-			_prepend.apply(self, arguments);
-			self.trigger("vgrid-added");
+		$self.prepend = function() {
+			prepend.apply($self, arguments);
+			$self.triggerHandler('vgrid-added');
 		};
 	};
+
 	$.fn.extend({
-		vgrid: function(option)
-		{
-			var _target = $(this);
-			var _opt = option || {};
-			if (_opt.easeing) {
-				_opt.easing = _opt.easeing;
+		vgrid: function(option) {
+			var $target = $(this);
+			var opt = $.extend({
+				time: 500,
+				delay: 0,
+				easing: 'linear',
+				autoResize: true
+			}, option);
+			if (opt.easeing) {
+				opt.easing = opt.easeing;
 			}
-			_target.each(function(){
-				var _self = $(this);
-				_self.data("_vgopt", _opt);
-				_self.data("_vgchild", _self.find("> *"));
-				_self.data("_vgdefchild", _self.data("_vgchild"));
-				_self.css({
-					"position": "relative",
-					"width": "auto"
+			$target.each(function() {
+				var $self = $(this);
+				var $child = $self.find('> *');
+				$self.data('_vgopt', opt);
+				$self.data('_vgchild', $child);
+				$self.data('_vgdefchild', $child);
+				$self.css({
+					position: 'relative'
 				});
-				_self.data("_vgchild").css("position", "absolute");
-				makePos(_self);
-				moveTo(_self.data("_vgchild"));
-				if(_self.data("_vgopt").fadeIn)
-				{
-					var _prop = (typeof(_self.data("_vgopt").fadeIn)=='object')
-									? _self.data("_vgopt").fadeIn
-									: {time: _self.data("_vgopt").fadeIn} ;
-					_self.data("_vgchild").each(function(i)
-					{
-						var _c = $(this);
-						if(_c.css("display") == "none"){
-							return true;
+				$child.css('position', 'absolute');
+				makePos($self);
+				moveTo($child);
+				if (opt.fadeIn) {
+					var $prop = (function() {
+						if (typeof(opt.fadeIn) === 'object') {
+							return opt.fadeIn;
+						} else {
+							return {
+								time: opt.fadeIn
+							};
 						}
-						_c.stop().css({opacity:0});
-						setTimeout(function(){
-							_c.stop().fadeTo(_prop.time || 250, 1);
-						}, i * (_prop.delay || 0));
+					})();
+
+					$child.each(function(i) {
+						var $c = $(this);
+						$c.css('display', 'none');
+						$c.stop().css({opacity: 0});
+						setTimeout(function() {
+							$c.stop().fadeTo($prop.time || 250, 1);
+						}, i * ($prop.delay || 0));
 					});
 				}
-				$(window).resize(function(e)
-				{
-					refreshHandler(_self);
+				$(window).resize(function(e) {
+					if (opt.autoResize !== true) {
+						return;
+					}
+					refreshHandler($self);
 				});
-				if(_opt.useLoadImageEvent) setImgLoadEvent(_self, refreshHandler);
-				if(_opt.useFontSizeListener) setFontSizeListener(_self, refreshHandler);
+				if (opt.useLoadImageEvent) {
+					setImgLoadEvent($self, refreshHandler);
+				}
+				if (opt.useFontSizeListener) {
+					setFontSizeListener($self, refreshHandler);
+				}
+
+				$self.triggerHandler('vgrid_init');
 			});
-			return _target;
+			return $target;
 		},
-		vgrefresh: function(easing, time, delay, func)
-		{
-			var _target = $(this);
-			_target.each(function(){
-				var _obj = $(this);
-				var _opt = _obj.data("_vgopt") || {};
-				if(_obj.data("_vgchild"))
-				{
-					_obj.data("_vgchild", _obj.find("> *"));
-					_obj.data("_vgchild").css("position", "absolute");
-					makePos(_obj);
-					time = typeof(time)=="number" ? time : _obj.data("_vgopt").time || 500;
-					delay = typeof(delay)=="number" ? delay : _obj.data("_vgopt").delay || 0;
-					animateTo(
-						_obj.data("_vgchild"),
-						easing || _obj.data("_vgopt").easing || "linear",
-						time,
-						delay
-					);
-					if(typeof(func)=='function')
-					{
-						setTimeout(
-							func,
-							_obj.data("_vgchild").length * delay + time
-						);
+		vgrefresh: function(easing, time, delay, func) {
+			var $target = $(this);
+			$target.each(function() {
+				var $obj = $(this);
+				var opt = $obj.data('_vgopt') || {};
+				if ($obj.data('_vgchild')) {
+					var $child = $obj.find('> *');
+					$obj.data('_vgchild', $child);
+					$child.css('position', 'absolute');
+					makePos($obj);
+					time = typeof(time) === 'number' ? time : opt.time;
+					delay = typeof(delay) === 'number' ? delay : opt.delay;
+					easing = easing || opt.easing;
+					animateTo($child, easing, time, delay);
+					if (typeof(func) === 'function') {
+						setTimeout(func, ($child.length * delay) + time);
 					}
 				}
-				if(_opt.useLoadImageEvent) setImgLoadEvent(_obj, refreshHandler);
-			});
-			return _target;
-		},
-		vgsort: function(func, easing, time, delay)
-		{
-			var _target = $(this);
-			_target.each(function(){
-				var _obj = $(this);
-				if(_obj.data("_vgchild"))
-				{
-					_obj.data("_vgchild", _obj.data("_vgchild").sort(func));
-					_obj.data("_vgchild").each(function(num){
-						$(this).appendTo(_obj);
-					});
-					makePos(_obj);
-					animateTo(
-						_obj.data("_vgchild"),
-						easing || _obj.data("_vgopt").easing || "linear",
-						typeof(time)=="number" ? time : _obj.data("_vgopt").time || 500,
-						typeof(delay)=="number" ? delay : _obj.data("_vgopt").delay || 0
-					);
+				if (opt.useLoadImageEvent) {
+					setImgLoadEvent($obj, refreshHandler);
 				}
 			});
-			return _target;
+			return $target;
+		},
+		vgsort: function(func, easing, time, delay) {
+			var $target = $(this);
+			$target.each(function() {
+				var $obj = $(this);
+				if ($obj.data('_vgchild')) {
+					var opt = $obj.data('_vgopt') || {};
+					var $child = $obj.data('_vgchild');
+
+					$obj.data('_vgchild', $child.sort(func));
+					$child.each(function(num) {
+						$(this).appendTo($obj);
+					});
+					makePos($obj);
+
+					easing = easing || opt.easing;
+					time = typeof(time) === 'number' ? time : opt.time;
+					delay = typeof(delay) === 'number' ? delay : opt.delay;
+					animateTo($child, easing, time, delay);
+				}
+			});
+			return $target;
+		},
+		vgheight: function() {
+			var $target = $(this);
+			return $target.data('_vgwrapheight');
 		}
 	});
 })(jQuery);
